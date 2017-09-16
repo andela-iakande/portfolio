cd8a85326fddff1d2dade87083b434dba5ef3704
Imisioluwa Akande <imisioluwa.akande@andela.com>
2017-09-16 16:23:54 +0100

    add contact page to frontend

--- src/components/about/AboutPage.js
+++ src/components/about/AboutPage.js
@@ -4,12 +4,37 @@ import { Link } from 'react-router';
 class AboutPage extends React.Component {
     render() {
         return (
-           <div className="jumbotron">
-             <h1>My portfolio </h1> 
-             <p> Know more about me </p>
-             <Link to ="/" className="btn btn-primary btn-lg"> Meet Me
-             </Link>
-           </div>         
+           
+              <div id ="contact" className="contact">
+                  <div className="container">
+                      <div className="row">
+                          <h2>Contact</h2>
+                          <p>Please kindly utilize this form to send us feedback. We will reply as as soon as possible</p>
+                          <div className="col-lg-6 col-md-6">
+                              <div className="input-group input-group-lg">
+                                  <span className="input-group-addon" id="sizing-addon1"><i className="fa fa-user" aria-hidden="true"></i></span>
+                                  <input type="text" className="form-control" aria-describedby="sizing-addon1" placeholder="Full Name"/>
+                              </div> 
+                              <div className="input-group input-group-lg">
+                                  <span className="input-group-addon" id="sizing-addon1"><i className="fa fa-envelope" aria-hidden="true"></i></span>
+                                  <input type="text" className="form-control" aria-describedby="sizing-addon1" placeholder="Email Address"/>
+                              </div> 
+                              <div className="input-group input-group-lg">
+                                  <span className="input-group-addon" id="sizing-addon1"><i className="fa fa-phone" aria-hidden="true"></i></span>
+                                  <input type="text" className="form-control" aria-describedby="sizing-addon1" placeholder="Phone Number"/>
+                              </div>         
+                          </div>
+                          <div className="col-lg-6 col-md-6">
+                              <div className="input-group">
+                                  <textarea name="" id="" cols="80" rows="6" className="form-control" placeholder="write us here..."></textarea>
+                              </div>    
+                              <button className="btn btn-lg">Submit your message</button>
+                          </div>    
+                      </div>    
+                  </div>   
+                </div>   
+             
+            
         );
     }
 }
