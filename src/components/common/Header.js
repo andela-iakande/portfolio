64f8f9e87205df300d1ecc8a39b6dc2b71af9b31
Imisioluwa Akande <imisioluwa.akande@andela.com>
2017-09-16 12:24:47 +0100

    fix carousel on home page only

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -29,38 +29,7 @@ const Header = () => {
                     </ul>  
                 </div>
             </div>
-            </nav> 
-            <div id="myCarousel" className="carousel slide" data-ride="carousel">
-                <ol className="carousel-indicators">
-                    <li data-target="#myCarousel" data-slide-to="0" className="active"></li>
-                    <li data-target="#myCarousel" data-slide-to="1"></li>
-                    <li data-target="#myCarousel" data-slide-to="2"></li>
-                </ol>
-
-                <div className="carousel-inner" role="listbox">
-                    <div className="item active">
-                        <img src="/public/snow.png" alt="Los Angeles"/>
-                    </div> 
-                    <div className="item">
-                        <img src="/public/red.png" alt="Los Angeles"/>
-                    </div> 
-                    <div className="item">
-                        <img src="/public/mountains.png" alt="Los Angeles"/>
-                    </div> 
-                    
-                </div>          
-                    <div className="carousel-caption">
-                        <button type= "button" className= "btn btn-default">Get Started</button>
-                    </div> 
-                    <Link to="#myCarousel" className="left carousel-control"  data-slide="prev">
-                        <span className="glyphicon glyphicon-chevron-left"></span>
-                        <span className="sr-only">Previous</span>
-                    </Link>
-                    <Link to="#myCarousel" className="right carousel-control"  data-slide="next">
-                        <span className="glyphicon glyphicon-chevron-right"></span>
-                        <span className="sr-only">Next</span>
-                    </Link>   
-                </div> 
+            </nav>  
             </header>     
     );
 };
--- src/components/home/HomePage.js
+++ src/components/home/HomePage.js
@@ -4,13 +4,39 @@ import { Link } from 'react-router';
 class HomePage extends React.Component {
     render() {
         return (
-            <div>
-             <h1> </h1> 
-             <h1>Welcome to My Home page </h1> 
-             <p> Navigate through my home page here </p>
-             <Link to ="products" className="btn btn-primary btn-lg"> check Products
-             </Link>
-             </div>
+                <div id="myCarousel" className="carousel slide" data-ride="carousel">
+                <ol className="carousel-indicators">
+                    <li data-target="#myCarousel" data-slide-to="0" className="active"></li>
+                    <li data-target="#myCarousel" data-slide-to="1"></li>
+                    <li data-target="#myCarousel" data-slide-to="2"></li>
+                </ol>
+
+                <div className="carousel-inner" role="listbox">
+                    <div className="item active">
+                        <img src="/public/snow.png" alt="Los Angeles"/>
+                    </div> 
+                    <div className="item">
+                        <img src="/public/red.png" alt="Los Angeles"/>
+                    </div> 
+                    <div className="item">
+                        <img src="/public/mountains.png" alt="Los Angeles"/>
+                    </div> 
+                    
+                </div>          
+                    <div className="carousel-caption">
+                    <Link to ="products" className="btn btn-primary btn-lg"> check Products
+                    </Link>
+                    </div> 
+                    <Link to="#myCarousel" className="left carousel-control"  data-slide="prev">
+                        <span className="glyphicon glyphicon-chevron-left"></span>
+                        <span className="sr-only">Previous</span>
+                    </Link>
+                    <Link to="#myCarousel" className="right carousel-control"  data-slide="next">
+                        <span className="glyphicon glyphicon-chevron-right"></span>
+                        <span className="sr-only">Next</span>
+                    </Link>   
+                </div>
+               
               
         );
     }
